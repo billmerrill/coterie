1e8762b6aba66b5c57530264156753e6b4c83ef3
Bill Merrill <evilai@gmail.com>
2017-02-07 12:12:15 -0800

    -project stars!

--- src/build_orion.py
+++ src/build_orion.py
@@ -1,3 +1,4 @@
+from math import sin, cos, sqrt, radians, degrees
 import sqlite3
 
 CONFIG = {
@@ -7,7 +8,8 @@ CONFIG = {
 
 class ConstellationPoint(object):
 
-    def __init__(self, hyg_id, designation, proper_name, ra, dec, distance, magnitude):
+    def __init__(self, hyg_id, designation, proper_name, ra, dec,
+                 distance, magnitude):
         self.hyg_id = hyg_id
         self.designation = designation or ""
         self.proper_name = proper_name or ""
@@ -15,13 +17,19 @@ class ConstellationPoint(object):
         self.dec = dec
         self.distance = distance or ""
         self.magnitude = magnitude or ""
+        self.projected = None
 
-    def get_key(self):
+    @property
+    def key(self):
         if self.proper_name:
             return self.proper_name
         if self.designation:
             return self.designation
         return self.hyg_id
+
+    def __str__(self):
+        return "<ConstellationPoint {:10} ra:{:8} dec:{:10} projection:{}>" \
+            .format(self.key, self.ra, self.dec, self.projected)
 
 
 class Constellation(object):
@@ -30,8 +38,14 @@ class Constellation(object):
         self.abbreviation = abbreviation
         self.stars = {}
 
+    def __str__(self):
+        stars = []
+        for s in sorted(self.stars):
+            stars.append(str(self.stars[s]))
+        return "<Constellation {} \n\t{}>".format(self.abbreviation, "\n\t".join(stars))
+
     def add_star(self, star):
-        self.stars[star.get_key()] = star
+        self.stars[star.key] = star
 
     def load_from_sqlite(self, star_db_file):
         conn = sqlite3.connect(star_db_file)
@@ -49,12 +63,15 @@ class Constellation(object):
         FROM stars WHERE
         constellation = :con AND
         bayer_flamsteed_designation != "" AND
-        magnitude < 10'''
+        magnitude < 3
+        ORDER BY id ASC'''
 
         for row in conn.execute(sql, {'con': self.abbreviation}):
+            print(row['ra'], row['dec'])
             self.add_star(ConstellationPoint(row['id'], row['designation'],
-                row['proper_name'], row['ra'], row['dec'], row['distance'],
-                row['magnitude']))
+                                             row['proper_name'], row['ra'],
+                                             row['dec'], row['distance'],
+                                             row['magnitude']))
 
     def find_center(self):
         min_ra = 500
@@ -62,7 +79,6 @@ class Constellation(object):
         min_dec = 100
         max_dec = -100
         for star in self.stars.values():
-            print(star.ra, star.dec)
             if star.ra < min_ra:
                 min_ra = star.ra
             if star.ra > max_ra:
@@ -75,13 +91,77 @@ class Constellation(object):
         mid_ra = (max_ra - min_ra) / 2
         mid_dec = (max_dec - min_dec) / 2
 
+        # return max_ra + mid_ra, max_dec + mid_dec
         return min_ra + mid_ra, min_dec + mid_dec
 
+    def project(self):
+        '''
+        http://www.projectpluto.com/project.htm
+
+        step 1 orthographic projection
+        delta_ra = ra - ra0;          /* determine difference in RA */
+
+        /* wlm added a -1 in code for looking out rather than down */
+        x1 = cos( dec) * sin( delta_ra);
+        y1 = sin( dec) * cos( dec0) - cos( dec) * cos( delta_ra) * sin( dec0);
+        '''
+        raC, decC = self.find_center()
+        raC = radians(raC * 15)
+        decC = radians(decC)
+
+        print("print center", raC, decC)
+        for star in self.stars.values():
+            ra = radians(star.ra * 15)
+            dec = radians(star.dec)
+            delta_ra = ra - raC
+            x1 = -1 * cos(dec) * sin(delta_ra)
+            y1 = sin(dec) * cos(decC) - cos(dec) * cos(delta_ra) * sin(decC)
+            z1 = sin(dec) * sin(decC) + cos(dec) * cos(decC) * cos(delta_ra)
+            if (z1 < -.9):
+                d = 20. * sqrt((1. - .81) / (1.00001 - z1 * z1))
+            else:
+                d = 2. / (z1 + 1.)
+            x = x1 * d
+            y = y1 * d
+            star.projected = (x, y)
+
+    def project_plate_carre(self):
+        raC, decC = self.find_center()
+        # raC, dec0 = 0,0
+        for s in sorted(self.stars):
+            ra = self.stars[s].ra
+            dec = self.stars[s].dec
+            delta_ra = ra - raC
+            x = delta_ra / sin(decC)
+            y = dec - decC
+            self.stars[s].projected = (x, y)
+
+
+def plot_stars(cons):
+    star_x = []
+    star_y = []
+    for s in sorted(cons.stars):
+        print(cons.stars[s].key)
+        projected = cons.stars[s].projected
+        if projected:
+            star_x.append(projected[0])
+            star_y.append(projected[1])
+
+    from bokeh.plotting import figure, output_file, show
+
+    output_file("stars.html")
+    # p = figure(plot_width=400, plot_height=400, title='orion')
+    p = figure(title='orion')
+    p.circle(star_x, star_y, size=10)
+    show(p)
 
 if __name__ == '__main__':
     abr = 'Ori'
+    # abr = 'UMa'
     orion = Constellation(abr, CONFIG)
     orion.load_from_sqlite(CONFIG['star_db'])
     ra, dec = orion.find_center()
-    print('mid')
-    print(ra, dec)
+    orion.project()
+    # orion.project_plate_carre()
+    print(orion)
+    plot_stars(orion)
