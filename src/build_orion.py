f28781c426a89892d766da425d80e0b402df36f2
Bill Merrill <evilai@gmail.com>
2017-02-08 11:26:04 -0800

    -select desired stars from db

--- src/build_orion.py
+++ src/build_orion.py
@@ -6,6 +6,50 @@ CONFIG = {
 }
 
 
+class ConstellationModel(object):
+
+    def __init__(self, constellation=None, stars=None, lines=None):
+        self.constellation = constellation
+        self.stars = stars
+        self.lines = lines
+
+    def chart2D(self):
+        # cons, stars, connections):
+        star_x = []
+        star_y = []
+        for s in sorted(self.constellation.stars):
+            print(self.constellation.stars[s].key)
+            projected = self.constellation.stars[s].projected
+            if projected:
+                star_x.append(projected[0])
+                star_y.append(projected[1])
+
+        from bokeh.plotting import figure, output_file, show
+        from bokeh.models import Label, WheelZoomTool, Segment
+
+        output_file("stars.html")
+        p = figure(plot_width=800, plot_height=800, title='orion')
+        p.toolbar.active_scroll = WheelZoomTool()
+        for star in self.constellation.stars.values():
+            if star.hyg_id in self.stars:
+                circle_color = "#000000"
+            else:
+                circle_color = "#cccccc"
+            p.circle(star.projected[0], star.projected[1],
+                     name=star.key, size=10, color=circle_color)
+            p.add_layout(Label(x=star.projected[0], y=star.projected[1],
+                               text="{}:{} {:.2f}".format(star.hyg_id, star.key,
+                               star.magnitude), text_color="#999999"))
+        for connection in self.lines:
+            a = self.constellation.get_star(connection[0])
+            b = self.constellation.get_star(connection[1])
+            glyph = Segment(x0=a.projected[0], y0=a.projected[1], x1=b.projected[0],
+                            y1=b.projected[1], line_color="#aaeeaa", line_width=4)
+            p.add_glyph(glyph)
+
+        show(p, browser="safari", new="window")
+
+
 class ConstellationPoint(object):
 
     def __init__(self, hyg_id, designation, proper_name, ra, dec,
@@ -36,8 +80,8 @@ class ConstellationPoint(object):
             .format(self.key, self.ra, self.dec, self.projected)
 
 
-
 class Constellation(object):
+
     def __init__(self, abbreviation, config, mag=4):
         self.config = config
         self.abbreviation = abbreviation
@@ -56,9 +100,36 @@ class Constellation(object):
     def get_star(self, index):
         return self.stars[index]
 
-    def load_from_sqlite(self, star_db_file):
+    def selection_sql(self, selection=None):
+        sql_str = None
+        sql_data = None
+        if selection:
+            sql_keys = []
+            sql_data = {}
+            for i, s in enumerate(selection):
+                key = 's{}'.format(i)
+                sql_keys.append(':'+key)
+                sql_data[key] = s
+
+            sql_str = "id in ({})".format(",".join(sql_keys))
+        else:
+            sql_str = '''
+            constellation = :con AND
+            bayer_flamsteed_designation != "" AND
+            magnitude < :mag'''
+            sql_data = {'con': self.abbreviation,
+                        'mag': self.magnitude_filter}
+
+        return sql_str, sql_data
+
+    def load_from_sqlite(self, star_db_file, selection=None):
         conn = sqlite3.connect(star_db_file)
         conn.row_factory = sqlite3.Row
+
+        # we are either grabbing all the stars of a certain character
+        # or just the stars in a list
+
+        selection_sql, selection_data = self.selection_sql(selection)
 
         sql = '''
         SELECT
@@ -70,13 +141,15 @@ class Constellation(object):
         dec,
         distance
         FROM stars WHERE
-        constellation = :con AND
-        bayer_flamsteed_designation != "" AND
-        magnitude < :mag
+        {selection_sql}
         ORDER BY id ASC'''
 
-        for row in conn.execute(sql, {'con': self.abbreviation,
-                                      'mag': self.magnitude_filter}):
+        sql = sql.format(selection_sql=selection_sql)
+
+        print(sql)
+        print(selection_data)
+
+        for row in conn.execute(sql, selection_data):
             print(row['ra'], row['dec'])
             self.add_star(ConstellationPoint(row['id'], row['designation'],
                                              row['proper_name'], row['ra'],
@@ -147,58 +220,26 @@ class Constellation(object):
             self.stars[s].projected = (x, y)
 
 
-def plot_stars(cons, stars, connections):
-    star_x = []
-    star_y = []
-    for s in sorted(cons.stars):
-        print(cons.stars[s].key)
-        projected = cons.stars[s].projected
-        if projected:
-            star_x.append(projected[0])
-            star_y.append(projected[1])
-
-    from bokeh.plotting import figure, output_file, show
-    from bokeh.models import Label, WheelZoomTool, Segment
 
 
-    output_file("stars.html")
-    # p = figure(plot_width=400, plot_height=400, title='orion')
-    p = figure(plot_width=800, plot_height=800,title='orion')
-    p.toolbar.active_scroll = WheelZoomTool()
-    for star in cons.stars.values():
-        if star.hyg_id in stars:
-            circle_color="#000000"
-        else:
-            circle_color="#cccccc"
-        p.circle(star.projected[0], star.projected[1], name=star.key, size=10, color=circle_color)
-        p.add_layout(Label(x=star.projected[0], y=star.projected[1], text="{}:{} {:.2f}".format(star.hyg_id, star.key, star.magnitude), text_color="#999999"))
-    for connection in connections:
-        a = cons.get_star(connection[0])
-        b = cons.get_star(connection[1])
-        glyph = Segment(x0=a.projected[0], y0=a.projected[1], x1=b.projected[0],
-                        y1=b.projected[1], line_color="#aaeeaa", line_width=4)
-        p.add_glyph(glyph)
-
-    # p.circle(star_x, star_y, size=10)
-    show(p, browser="safari", new="window")
 
 if __name__ == '__main__':
+
     abr = 'Ori'
-    # abr = 'UMa'
-    orion = Constellation(abr, CONFIG, mag=5)
-    orion.load_from_sqlite(CONFIG['star_db'])
-    ra, dec = orion.find_center()
-    orion.project()
-    # orion.project_plate_carre()
-    print(orion)
     orion_points = [27298, 24378, 27919, 25273, 26662, 26246, 25865, 26142, 26176,
                     22396, 22496, 22744, 23069, 22456, 22792, 22904, 23552, 28543,
                     29353, 28966, 28645, 27844]
     orion_lines = [(27298, 26662), (24378, 25865), (25865, 26246), (26246, 26662),
-                    (26246, 26176), (26662, 27919), (27919, 26142), (26142,25273),
-                    (25273, 27919),(25273, 22396), (22396, 22496), (22744, 22496),
-                    (23069, 22744), (22396, 22456), (22456, 22792), (22792, 22904),
-                    (22904, 23552), (27919, 28543), (28543, 29353), (28543, 28966),
-                    (29353, 28966), (29353, 28645), (28966, 27844), (28645, 27844),
-                    (25273, 25865)]
-    plot_stars(orion, orion_points, orion_lines)
+                   (26246, 26176), (26662, 27919), (27919, 26142), (26142, 25273),
+                   (25273, 27919), (25273, 22396), (22396, 22496), (22744, 22496),
+                   (23069, 22744), (22396, 22456), (22456, 22792), (22792, 22904),
+                   (22904, 23552), (27919, 28543), (28543, 29353), (28543, 28966),
+                   (29353, 28966), (29353, 28645), (28966, 27844), (28645, 27844),
+                   (25273, 25865)]
+
+    orion = Constellation(abr, CONFIG, mag=5)
+    orion.load_from_sqlite(CONFIG['star_db'], selection=orion_points)
+    orion.project()
+    print(orion)
+    orion_model = ConstellationModel(constellation=orion, stars=orion_points, lines=orion_lines)
+    orion_model.chart2D()
